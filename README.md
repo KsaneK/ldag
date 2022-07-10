29e3878edd1b13afe48d177f43422c9959faaf83
Łukasz Olender <xaneqtv@gmail.com>
2022-07-10 11:23:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Light Directed Acyclic Graph (LDAG)
+This library allows to define and run DAGs.  
+It requires RabbitMQ for communication (triggering DAGs, data transfer between tasks).
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+INSTALL_REQUIRES = []
+DEV_INSTALL_REQUIRES = []
+
+setup(
+    name="LDAG",
+    description="Light Directed Acyclic Graph",
+    author="Lukasz Olender",
+    author_email="olender.lukasz96@gmail.com",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    extra_require={"dev": DEV_INSTALL_REQUIRES}
+)
