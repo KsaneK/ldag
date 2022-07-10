05dccf12bfe04dbe691236e3ffa59f1cf0d7abb0
Łukasz Olender <xaneqtv@gmail.com>
2022-07-10 21:06:37 +0200

    Restore params to PythonTask class

--- ldag/tasks/python_task.py
+++ ldag/tasks/python_task.py
@@ -1,13 +1,14 @@
-from typing import Callable
+from typing import Callable, Any, Dict
 
 from ldag.tasks import BaseTask
 
 
 class PythonTask(BaseTask):
-    def __init__(self, func: Callable, *args, **kwargs):
+    def __init__(self, func: Callable, params: Dict[str, Any], *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._func = func
+        self._params = params
 
     def execute(self):
-        self._func()
+        self._func(**self._params)
