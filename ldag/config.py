2ec7ab8cdb71c648bd28b42d79b0708e2deea3e2
Łukasz Olender <xaneqtv@gmail.com>
2022-07-10 14:34:41 +0200

    Add rabbitmq section in ldag config

--- ldag/config.py
+++ ldag/config.py
@@ -1,11 +1,29 @@
 import configparser
 
-__all__ = ["app_id", "dags_directory"]
+__all__ = [
+    "app_id",
+    "dags_directory",
+    "rabbitmq_host",
+    "rabbitmq_port",
+    "rabbitmq_user",
+    "rabbitmq_password"
+]
 
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(allow_no_value=True)
 config.read("ldag_config")
 app_id = config.get("default", "APP_ID")
 dags_directory = config.get("default", "DAGS_DIRECTORY")
 dags_directory = dags_directory.replace("/", ".")
 if dags_directory.endswith("."):
     dags_directory = dags_directory[:-1]
+
+rabbitmq_host = config.get("rabbitmq", "host")
+rabbitmq_port = config.getint("rabbitmq", "port")
+rabbitmq_user = config.get("rabbitmq", "user")
+rabbitmq_password = config.get("rabbitmq", "password")
+
+if rabbitmq_user == "":
+    rabbitmq_user = None
+
+if rabbitmq_password == "":
+    rabbitmq_password = None
